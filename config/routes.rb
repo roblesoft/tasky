07de972ea68732350442ab8a572af8d4f3ad8628
Uriel Robles <urielrobles802@gmail.com>
2021-01-09 17:11:35 -0600

    default root and new session

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'homes#newsfeed'
   resources :projects
   resources :homes do
     collection do
       get :login
       get :newsfeed
+      post :new_session
     end
   end
 end
