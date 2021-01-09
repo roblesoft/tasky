135d3780575684454ad7a4ac833389c4f9f9ab85
Uriel Robles <urielrobles802@gmail.com>
2021-01-09 17:10:40 -0600

    user authentication firewall

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
+# application controller
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  def authenticate_user!
+    redirect_to login_homes_path unless current_user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
