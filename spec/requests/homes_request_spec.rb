3ed3f6f259a6143485fea6e597c510fb79d4b89f
Uriel Robles <urielrobles802@gmail.com>
2021-01-09 13:20:11 -0600

    creation of home controller

---
+++ spec/requests/homes_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+
+  describe "GET /login" do
+    it "returns http success" do
+      get "/homes/login"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /newsfeed" do
+    it "returns http success" do
+      get "/homes/newsfeed"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
