4a9733d51ea93e1e27903c18bc68e0c1de83d0fa
Uriel Robles <urielrobles802@gmail.com>
2021-01-09 17:10:55 -0600

    user login and new session

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
+# homes controller
 class HomesController < ApplicationController
-  def login
-  end
+  before_action :authenticate_user!, except: %i[login new_session]
+
+  def login; end
 
   def newsfeed; end
+
+  def new_session
+    user = User.find_by(email: params[:log_in][:email])
+
+    if user
+      session[:user_id] = user.id
+      flash[:success] = 'The login was successfully completed'
+    else
+      flash[:error] = 'User not found'
+    end
+    redirect_to newsfeed_homes_path
+  end
 end
