a635c7bb4743314d0ef5cddace2c3583522cfb0f
Uriel Robles <urielrobles802@gmail.com>
2021-01-10 16:43:27 -0600

    newfeed colors

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
+
+# projects helper
 module ProjectsHelper
+  def news_color(type)
+    case type
+    when 'create'
+      'primary'
+    when 'update'
+      'success'
+    when 'delete'
+      'danger'
+    else
+      'info'
+    end
+  end
 end
