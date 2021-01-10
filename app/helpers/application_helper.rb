590008505e59198d68406f3018dd9b7a465b91b9
Uriel Robles <urielrobles802@gmail.com>
2021-01-09 21:43:01 -0600

    assign owner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
+# module for application helper
 module ApplicationHelper
+  def render_modal_content(title, model, &block)
+    locals = { title: title, model: model, block: block }
+    render partial: 'layouts/modal', locals: locals
+  end
 end
