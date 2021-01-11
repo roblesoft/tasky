85bbadc430932e09e1e837462d9e7188c4810175
Uriel Robles <urielrobles802@gmail.com>
2021-01-11 11:07:19 -0600

    ui improvments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,4 +18,8 @@ require("channels")
 
 import '../scss/application.scss'
 
+import Swal from 'sweetalert2'                                                  
+import 'sweetalert2/src/sweetalert2.scss'                                       
+window.Swal = Swal   
+
 import "controllers"
